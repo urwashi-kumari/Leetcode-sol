160c84e82c82e8bf25ed75d64fa217c3c13b7fb4
URWASHI <153119972+urwashi-kumari@users.noreply.github.com>
2026-08-01 15:30:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (63.26%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> freq(26, 0);
+        vector<bool> seen(26, false);
+        
+        for(char &x : s){
+            freq[x - 'a']++;
+        }
+        
+        string res = "";
+        for(char &x : s){
+            freq[x - 'a']--;
+            
+            if(seen[x - 'a']){
+                continue;
+            }
+            while(!res.empty() && res.back() > x && freq[res.back() - 'a'] > 0){
+                seen[res.back() - 'a'] = false;
+                res.pop_back();
+            }
+            
+            res += x;
+            seen[x - 'a'] = true;
+        }
+        
+        return res;
+    }
+};
